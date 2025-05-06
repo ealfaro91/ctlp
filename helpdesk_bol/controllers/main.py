ed0f9a0b1c8ca964ac49c4959eeb1aed189926f1
Elymar Alfaro <elymar.alfaro@gmail.com>
2025-05-06 11:20:11 -0500

    [RUTAS]

--- helpdesk_bol/controllers/main.py
+++ helpdesk_bol/controllers/main.py
@@ -144,7 +144,7 @@ class Home(home.Home):
              # Manually authenticate the user
             _logger.info("Logging in: %s", user.password)
             request.session.authenticate(request.db, login, login)
-            if redirect.startswith('/web/login'):
+            if not redirect or 'web/login' in redirect:
                 redirect = '/help_desk'
             return request.redirect(self._login_redirect(user.id, redirect=redirect))
 
