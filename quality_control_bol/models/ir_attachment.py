a40220abb017f3f8e57c6a8072fb25ee1ff0e52e
Elymar Alfaro <elymar.alfaro@gmail.com>
2025-11-28 15:09:51 -0500

    [FIX]

--- quality_control_bol/models/ir_attachment.py
+++ quality_control_bol/models/ir_attachment.py
@@ -238,7 +238,7 @@ class IrAttachment(models.Model):
             #         mail_template = self.env.ref(
             #             "project_bol.fsn_approved_notification", raise_if_not_found=True
             #         )
-            #         mail_template.sudo().send_mail(fsn.id, force_send=False, raise_exception=True)
+            #         mail_template.sudo().send_mail(fsn.id, force_send=True, raise_exception=True)
             #         fsn._action_create_project()
 
     def button_publish_document(self):
@@ -251,7 +251,7 @@ class IrAttachment(models.Model):
             )
             mail_template.write({"email_to": user.email})
             mail_template.send_mail(
-                self.id, force_send=False, raise_exception=True
+                self.id, force_send=True, raise_exception=True
             )
         self.state = 'published'
         return {
@@ -345,7 +345,7 @@ class IrAttachment(models.Model):
         )
         mail_template.write({"email_to": user.email})
         mail_template.send_mail(
-            self.id, force_send=False, raise_exception=True
+            self.id, force_send=True, raise_exception=True
         )
         return {
             'type': 'ir.actions.client',
@@ -392,7 +392,7 @@ class IrAttachment(models.Model):
                 )
                 mail_template.write({"email_to": user.email})
                 mail_template.send_mail(
-                    rec.id, force_send=False, raise_exception=True
+                    rec.id, force_send=True, raise_exception=True
                 )
             for log in rec.approval_log_ids:
                 log.request_sign_date = fields.Datetime.now()
